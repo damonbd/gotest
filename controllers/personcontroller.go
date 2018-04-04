984b7a6c72414e81c77b9682597cc956a21d1c03
Damon <damonbd@gmail.com>
2018-04-03 18:01:02 -0700

    refactor: Moves all routes to personcontroller. Cleans up naming.

--- controllers/personcontroller.go
+++ controllers/personcontroller.go
@@ -1,16 +1,23 @@
 package personcontroller
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"rolljimmy/models"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 var tpl *template.Template
 
+var people []models.Person
+
 func init() {
-	//tpl = template.Must(template.ParseGlob("./templates/*"))
+	people = append(people, models.Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &models.Address{City: "City X", State: "State X"}})
+	people = append(people, models.Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &models.Address{City: "City Z", State: "State Y"}})
+	people = append(people, models.Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
 }
 
 func personcontroller() {
@@ -21,13 +28,22 @@ func personcontroller() {
 func AddRoutes(r *mux.Router, mainTpl *template.Template) {
 	tpl = mainTpl
 
-	r.HandleFunc("/differentcontroller", DifferentController)
-}
+	r = r.PathPrefix("/person").Subrouter()
 
-//AddSubRoutes ...
-func AddSubRoutes(r *mux.Router) {
-	r = r.PathPrefix("/subroute").Subrouter()
-	r.HandleFunc("/testsubroute", testsubroute)
+	r.HandleFunc("/testsubroute", TestSubroute)
+	r.HandleFunc("/differentcontroller", DifferentController)
+	r.HandleFunc("/people", GetPeople).Methods("GET")
+	r.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	r.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	r.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	r.HandleFunc("/KruthSucks", KruthSucks)
+	r.HandleFunc("/testHtml", TestHTML)
+	r.HandleFunc("/passdata", PassData)
+	r.HandleFunc("/passperson", PassPerson)
+	r.HandleFunc("/createpersonform", CreatePersonForm)
+	r.HandleFunc("/postaperson", PostAPerson)
+	r.HandleFunc("/passpeople", PassPeople)
+	r.HandleFunc("/uppercasestring", UppercaseString)
 }
 
 //DifferentController ...
@@ -35,12 +51,96 @@ func DifferentController(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "differentcontroller.html", nil)
 }
 
-func testsubroute(w http.ResponseWriter, r *http.Request) {
+//TestSubroute ...
+func TestSubroute(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "testsubroute.html", nil)
 }
 
-//Test ...
-func Test() {
-	x := 2
+//KruthSucks ...
+func KruthSucks(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "kruthsucks.gohtml", nil)
+}
+
+//TestHTML ...
+func TestHTML(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "testHtml.html", nil)
+}
+
+//PassData ...
+func PassData(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "passdata.html", "this string is data passed from my func main in main.go")
+}
+
+//PassPerson ...
+func PassPerson(w http.ResponseWriter, r *http.Request) {
+	person := people[0]
+
+	tpl.ExecuteTemplate(w, "passperson.html", person)
+}
+
+//CreatePersonForm ...
+func CreatePersonForm(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "createpersonform.html", nil)
+}
+
+//PostAPerson ...
+func PostAPerson(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/people", http.StatusSeeOther)
+		return
+	}
+	x := len(people)
 	x = x + 1
+
+	people = append(people, models.Person{ID: strconv.Itoa(x), Firstname: r.FormValue("Firstname"), Lastname: r.FormValue("Lastname")})
+
+	json.NewEncoder(w).Encode(people)
+}
+
+//PassPeople ...
+func PassPeople(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "passpeople.html", people)
+}
+
+//GetPeople does things
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+//UppercaseString ...
+func UppercaseString(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "uppercasestring.html", "this text should be changed to uppercase.")
+}
+
+//GetPerson does things
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+//CreatePerson does things
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person models.Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+
+//DeletePerson does things
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
 }
--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
 
 	//local
-
 	"rolljimmy/controllers"
-	"rolljimmy/models"
 )
 
 var tpl *template.Template
 var router *mux.Router
-var subRouter *mux.Router
-var people []models.Person
 
+//probably move to another file
 var fm = template.FuncMap{
 	"uc": strings.ToUpper,
 }
@@ -31,121 +26,12 @@ func init() {
 }
 
 func main() {
-
-	people = append(people, models.Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &models.Address{City: "City X", State: "State X"}})
-	people = append(people, models.Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &models.Address{City: "City Z", State: "State Y"}})
-	people = append(people, models.Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
-	addRoutes()
-
+	addControllerRoutes()
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-//addRoutes ...
-func addRoutes() {
+//addControllerRoutes ...
+func addControllerRoutes() {
 	router = mux.NewRouter()
 	personcontroller.AddRoutes(router, tpl)
-	personcontroller.AddSubRoutes(router)
-
-	router.HandleFunc("/people", GetPeople).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-	router.HandleFunc("/KruthSucks", KruthSucks)
-	router.HandleFunc("/testHtml", TestHTML)
-	router.HandleFunc("/passdata", PassData)
-	router.HandleFunc("/passperson", PassPerson)
-	router.HandleFunc("/createpersonform", CreatePersonForm)
-	router.HandleFunc("/postaperson", PostAPerson)
-	router.HandleFunc("/passpeople", PassPeople)
-	router.HandleFunc("/uppercasestring", UppercaseString)
-}
-
-//KruthSucks ...
-func KruthSucks(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "kruthsucks.gohtml", nil)
-}
-
-//TestHTML ...
-func TestHTML(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "testHtml.html", nil)
-}
-
-//PassData ...
-func PassData(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "passdata.html", "this string is data passed from my func main in main.go")
-}
-
-//PassPerson ...
-func PassPerson(w http.ResponseWriter, r *http.Request) {
-	person := people[0]
-
-	tpl.ExecuteTemplate(w, "passperson.html", person)
-}
-
-//CreatePersonForm ...
-func CreatePersonForm(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "createpersonform.html", nil)
-}
-
-//PostAPerson ...
-func PostAPerson(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Redirect(w, r, "/people", http.StatusSeeOther)
-		return
-	}
-	x := len(people)
-	x = x + 1
-
-	people = append(people, models.Person{ID: strconv.Itoa(x), Firstname: r.FormValue("Firstname"), Lastname: r.FormValue("Lastname")})
-
-	json.NewEncoder(w).Encode(people)
-}
-
-//PassPeople ...
-func PassPeople(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "passpeople.html", people)
-}
-
-//GetPeople does things
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-
-//UppercaseString ...
-func UppercaseString(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "uppercasestring.html", "this text should be changed to uppercase.")
-}
-
-//GetPerson does things
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-}
-
-//CreatePerson does things
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var person models.Person
-	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-}
-
-//DeletePerson does things
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(people)
-	}
 }
